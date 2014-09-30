62ea3ee05400d39a34380fb4665e34e0043d179a
南山 <junjiemars@gmail.com>
2014-09-30 23:07:27 +0800

    init groovy-study project

---
+++ src/Callee.groovy
@@ -0,0 +1,8 @@
+/**
+ * Created by junjie on 30/9/14.
+ */
+class Callee {
+    void hello() {
+        println 'Hello,world'
+    }
+}
---
+++ src/Caller.groovy
@@ -0,0 +1,5 @@
+/**
+ * Created by junjie on 30/9/14.
+ */
+c = new Callee()
+c.hello()
